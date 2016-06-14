c7c8beb50dd2462e94894461cf8579376b74c0b4
zszszsz <zszszsz@outlook.com>
2016-06-14 13:35:19 +0800

    new file:   adds.js 	modified:   index.js 	modified:   package.json

---
+++ adds.js
@@ -0,0 +1,1 @@
+//address-location table
--- index.js
+++ index.js
@@ -2,33 +2,47 @@
 'use strict';
 const nets = require('os').networkInterfaces;
 const cp = require('child_process');
-if(process.getuid){
-	if(process.getuid()==0)
-		loop();
-	else{
-		console.error('please run as root');
-		process.exit(1);
-	}
-}
-else{
-	cp.exec('sfc',(r,s,e)=>{
-		if(s.length<70){
-			console.error('please run administritive');
+
+
+
+const args = require('yargs')
+	.boolean(['no_check_root','no_check_admin'])
+	.option('r',{
+		alias:'rm_prefix',
+		demand:true,
+		default:'2001:250:401:',
+		type:'string'})
+	.option('p',{
+		alias:'prefix',
+		demand:true,
+		default:'2001:250:401:3610:',
+		type:'string'})
+	.argv;
+
+if(args.no_check_root==false&&args.no_check_admin==false){
+	if(process.getuid){
+		if(process.getuid()!=0){
+			console.error('please run as root');
 			process.exit(1);
 		}
-		else loop();
-	});
-
+	}
+	else{
+		cp.exec('sfc',(r,s,e)=>{
+			if(s.length<70){
+				console.error('please run administritive');
+				process.exit(1);
+			}
+		});
+	}
 }
-
 function loop(){
 	console.log('\x1Bc');
 	var devs=nets();
 	for(var dev in devs){
 		for(var add of devs[dev]){
 			if(add.family!='IPv6')continue;
-			if(add.address.search(/2001:250:401:/)==-1)continue;
-			if(add.address.search(/2001:250:401:3610/)!=-1){
+			if(add.address.search(RegExp(args.rm_prefix))==-1)continue;
+			if(add.address.search(RegExp(args.prefix))!=-1){
 				console.log('using address '+add.address);
 				continue;
 			}
