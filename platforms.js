6d86220c15a9edb597c41bb0fa12b7799750503a
zszszsz <zszszsz@outlook.com>
2016-07-07 22:08:05 +0800

    modified:   platforms.js

--- platforms.js
+++ platforms.js
@@ -15,7 +15,21 @@ module.exports={
             if(callback)exec('ip -6 address del '+ip+'/64 dev '+dev,(e,so,se)=>{callback(e,so,se,payload)});
             else exec('ip -6 address del '+ip+'/64 dev '+dev);
         },
-        test:test
+        test:(callback,payload)=>{
+            request({
+                host:'2001:250:401:44::130',
+                family:6,
+                localAddress:payload.ip,
+                agent:false,
+                timeout:50
+            },()=>{
+                console.log('ip usable',payload.ip)
+                callback(payload,true);
+            }).on('error',()=>{
+                console.log('ip unusable',payload.ip)
+                callback(payload,false);
+            }).setTimeout(50).end();
+        }
     },
     win32:{
         add:(ip,dev,callback,payload)=>{
@@ -28,23 +42,21 @@ module.exports={
             if(callback)exec('powershell remove-netipaddress '+ip+' -InterfaceAlias '+dev+' -confirm:$false',(e,so,se)=>{callback(e,so,se,payload)}).stdin.end();
             else exec('powershell remove-netipaddress '+ip+' -InterfaceAlias '+dev+' -confirm:$false').stdin.end();
         },
-        test:test
+        test:(callback,payload)=>{
+            execSync('powershell get-netipaddress '+payload.ip);
+            request({
+                host:'2001:250:401:44::130',
+                family:6,
+                localAddress:payload.ip,
+                agent:false,
+                timeout:50
+            },()=>{
+                console.log('ip usable',payload.ip)
+                callback(payload,true);
+            }).on('error',()=>{
+                console.log('ip unusable',payload.ip)
+                callback(payload,false);
+            }).setTimeout(50).end();
+        }
     }
-}
-
-function test(callback,payload){
-    execSync('powershell get-netipaddress '+payload.ip);
-    request({
-        host:'2001:250:401:44::130',
-        family:6,
-        localAddress:payload.ip,
-        agent:false,
-        timeout:50
-    },()=>{
-        console.log('ip usable',payload.ip)
-        callback(payload,true);
-    }).on('error',()=>{
-        console.log('ip unusable',payload.ip)
-        callback(payload,false);
-    }).setTimeout(50).end();
 }
